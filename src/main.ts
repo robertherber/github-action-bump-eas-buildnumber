891ebac96ef018bcd32db83a912c077cf4f8c4a1
Robert Herber <robert@kingstinct.com>
2021-05-23 10:37:01 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
 import * as core from '@actions/core'
+import fs from 'fs'
 import {wait} from './wait'
+import detectIndent from 'detect-indent'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const filepath: string = core.getInput('filepath')
+    const platformsStr: string = core.getInput('platforms')
+
+    const platforms = platformsStr.toLowerCase().split(',') as (
+      | 'android'
+      | 'ios'
+    )[]
+
+    const jsonStr = fs.readFileSync(filepath, 'utf-8')
+
+    const indent = detectIndent(jsonStr).indent || '    '
+
+    const json = JSON.parse(jsonStr)
+
+    if (platforms.includes('android')) {
+      const versionCode = json.expo.android.versionCode as number
+      json.expo.android.versionCode = versionCode + 1
+    }
+
+    if (platforms.includes('ios')) {
+      const buildNumberStr = json.expo.ios.buildNumber as string
+      const buildNumber = parseInt(buildNumberStr)
+      json.expo.ios.buildNumber = buildNumber + 1
+    }
+
+    fs.writeFileSync(filepath, JSON.stringify(json, null, indent))
+
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
     core.debug(new Date().toTimeString())
